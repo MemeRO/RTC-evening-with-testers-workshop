a151cbd2a27dbe45eaafbd7d3726365971f9a235
Razvan Ariciuc <razvan.t.ariciuc@gmail.com>
2020-03-05 12:37:45 +0200

    Add negative test

--- src/test/java/TestEWT.java
+++ src/test/java/TestEWT.java
@@ -1,19 +1,18 @@
-import static org.junit.Assert.*;
-
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestEWT {
 
@@ -27,43 +26,42 @@ public class TestEWT {
     }
 
     @Test
-    public void RegisterUser() throws InterruptedException {
-        int randonNB = generateRandomInt(1000);
+    public void registerUser() throws InterruptedException {
         driver.get("http://89.33.132.18:8787/");
         assertEquals("EmpApp", driver.getTitle());
 
         driver.findElement(By.xpath("//input[@placeholder='email']")).sendKeys("aaa@aaa.com");
         driver.findElement(By.xpath("//input[@placeholder='password']")).sendKeys("aaaaa");
         driver.findElement(By.xpath("//button/span[text()='Login']")).click();
-        //Thread.sleep(3000);
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='welcome']")));
-        assertEquals("Welcome: aaa@aaa.com", driver.findElement(By.xpath("//div[())]")));
-
+        assertEquals("Welcome: aaa@aaa.com", driver.findElement(By.xpath("//div[@class='welcome']")).getText());
 
         driver.findElement(By.xpath("//button/span[text()='New user']")).click();
         assertEquals("< back", driver.findElement(By.xpath("//app-employee-form/button/span[@class='mat-button-wrapper']")).getText());
 
+        int randonNB = generateRandomInt(1000);
         driver.findElement(By.xpath("//input[@placeholder='first name']")).sendKeys("lalala" + randonNB);
         driver.findElement(By.xpath("//input[@placeholder='last name']")).sendKeys("tra");
         driver.findElement(By.xpath("//span[text()='I agree with the terms']")).click();
         driver.findElement(By.xpath("//button/span[text()='Submit']")).click();
 
-        List<WebElement> rows = driver.findElements(By.xpath("//table/tbody/tr"));
-        System.out.println(rows.size());
-        assertEquals("lalala" + randonNB, driver.findElement(By.xpath("//table/tbody/tr[" + rows.size() + "]/td[1]")).getText());
+        String expectedFirstName = "lalala" + randonNB;
+        assertTrue(expectedFirstName.contains(driver.findElement(By.xpath("//tbody/tr[last()]/td[1]")).getText()));
+    }
 
-        driver.findElement(By.xpath("//table/tbody/tr[" + rows.size() + "]/td/button/span[text()=' del']")).click();
-        ;
-//Thread.sleep(1000);
-        driver.findElement(By.xpath("//button/span[text()='Yes']")).click();
-//Thread.sleep(1000);
-// wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//app-snack/span")));
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//app-snack/span")));
-        List<WebElement> currentRows = driver.findElements(By.xpath("//table/tbody/tr"));
+    @Test
+    public void loginWithInvalidUser() {
+        driver.get("http://89.33.132.18:8787/");
+        assertEquals("EmpApp", driver.getTitle());
 
-        assertEquals(rows.size() - 1, currentRows.size());
+        driver.findElement(By.xpath("//input[@placeholder='email']")).sendKeys("bbb@aaa.com");
+        driver.findElement(By.xpath("//input[@placeholder='password']")).sendKeys("aaaaa");
+        driver.findElement(By.xpath("//button/span[text()='Login']")).click();
+
+        String actualErrorMessage = driver.findElement(By.xpath("//snack-bar-container/app-snack/span")).getText();
+        assertTrue(actualErrorMessage.contains("Problem signing in"));
     }
 
     @AfterClass
